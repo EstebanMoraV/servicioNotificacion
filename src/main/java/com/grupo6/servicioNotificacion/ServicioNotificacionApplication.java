43ffb1fd154aafb199a6b1c38377264cc17c89e5
Esteban <esteban.morauc@gmail.com>
2025-05-21 20:56:44 -0400

    Creación del microservicio Notificacion

---
+++ src/main/java/com/grupo6/servicioNotificacion/ServicioNotificacionApplication.java
@@ -0,0 +1,13 @@
+package com.grupo6.servicioNotificacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioNotificacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioNotificacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo6/servicioNotificacion/ServicioNotificacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo6.servicioNotificacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioNotificacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

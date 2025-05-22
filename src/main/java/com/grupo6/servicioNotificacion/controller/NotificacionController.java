e3b4ca16729b0b725d9187cbb04f060a2bd259b3
Esteban <esteban.morauc@gmail.com>
2025-05-21 23:04:50 -0400

    Prueba Final

--- src/main/java/com/grupo6/servicioNotificacion/controller/NotificacionController.java
+++ src/main/java/com/grupo6/servicioNotificacion/controller/NotificacionController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,8 +31,8 @@ public class NotificacionController {
         return new ResponseEntity<>(notificaciones, HttpStatus.OK);
     }
 
-    @GetMapping("/usuario")
-    public ResponseEntity<List<Notificacion>> getNotificacionesByUsuarioId(@RequestParam Integer usuarioId) {
+    @GetMapping("/usuario/{usuarioId}")
+    public ResponseEntity<List<Notificacion>> getNotificacionesByUsuarioId(@PathVariable Integer usuarioId) {
         List<Notificacion> notificaciones = notificacionService.getNotificacionesByUsuarioId(usuarioId);
         return new ResponseEntity<>(notificaciones, HttpStatus.OK);
     }
--- src/main/java/com/grupo6/servicioNotificacion/repository/UsuarioRepository.java
+++ src/main/java/com/grupo6/servicioNotificacion/repository/UsuarioRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.grupo6.servicioNotificacion.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-    Optional<Usuario> findByEmail(String email); // Método para buscar un usuario por su email
+    @SuppressWarnings("override")
+    Optional<Usuario> findById(Integer id);
 }
